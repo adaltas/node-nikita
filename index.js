3272b94641d7c676287c3d4cc700b79d7685bffe
David Worms <david@adaltas.com>
2013-07-08 12:12:17 +0200

    misc function accessible through `require('mecano').misc`

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ require('coffee-script');
 
 var mecano = require('./lib/mecano');
 var misc = require('./lib/misc');
-for(var k in misc){
-    mecano[k] = misc[k];
-}
+// for(var k in misc){
+//     mecano[k] = misc[k];
+// }
 module.exports = mecano;
+module.exports.misc = misc;
