5f74eb3ed42d7ed02a4cbf67299929899f1131f8
David Worms <david@adaltas.com>
2014-09-05 17:09:17 +0200

    remove index at package root

--- index.js
+++
@@ -1,8 +0,0 @@
-
-var mecano = require('./lib/mecano');
-var misc = require('./lib/misc');
-// for(var k in misc){
-//     mecano[k] = misc[k];
-// }
-module.exports = mecano;
-module.exports.misc = misc;
