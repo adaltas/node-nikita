f290373c00545f8729eb5c3dc7988aa66dc6b883
David Worms <david@adaltas.com>
2019-02-19 16:36:13 +0100

    tools.ssh.keygen: new key_format option

--- packages/tools/lib/ssh/keygen.js
+++ packages/tools/lib/ssh/keygen.js
@@ -9,6 +9,9 @@
 //   Specifies the number of bits in the key to create.
 // * `comment` (string, optional)   
 //   Comment such as a name or email.
+// * `key_format` (string, optional)   
+//   Specify a key format. The supported key formats are: `RFC4716` (RFC 4716/SSH2 public or
+//   private key), `PKCS8` (PEM PKCS8 public key) or `PEM` (PEM public key).
 // * `passphrase` (string, optional, "")
 //   Key passphrase, empty string for no passphrase.
 // * `target` (string, required)   
@@ -16,20 +19,24 @@
 // * `type` (string, optional, "rsa")   
 //   Type of key to create.
 
-// ## Exemple
+// ## Force the generation of a key compatible with SSH2
+
+// For exemple in OSX Mojave, the default export format is RFC4716.
 
 // ```
 // require('nikita')
 // .tools.ssh.keygen({
 //   bits: 2048,
 //   comment: 'my@email.com'
-//   target: './id_rsa'
+//   target: './id_rsa',
+//   key_format: 'PEM'
 // })
 
 // ## Source code
 var path;
 
 module.exports = function({options}) {
+  var ref;
   if (options.bits == null) {
     options.bits = 4096;
   }
@@ -38,6 +45,12 @@ module.exports = function({options}) {
   }
   if (options.passphrase == null) {
     options.passphrase = '';
+  }
+  if (options.key_format == null) {
+    options.key_format = null;
+  }
+  if (options.key_format && ((ref = options.key_format) !== 'RFC4716' && ref !== 'PKCS8' && ref !== 'PEM')) {
+    throw Error(`Invalid Option: key_format must be one of RFC4716, PKCS8 or PEM, got ${JSON.stringify(options.key_format)}`);
   }
   if (!options.target) {
     // Validation
@@ -53,6 +66,7 @@ module.exports = function({options}) {
       "-q", // Silence
       `-t ${options.type}`,
       `-b ${options.bits}`,
+      options.key_format ? `-m ${options.key_format}` : void 0,
       options.comment ? `-C '${options.comment.replace('\'',
       '\\\'')}'` : void 0,
       `-N '${options.passphrase.replace('\'',
--- packages/tools/src/ssh/keygen.coffee.md
+++ packages/tools/src/ssh/keygen.coffee.md
@@ -9,6 +9,9 @@ Generates keys for use by SSH protocol version 2.
   Specifies the number of bits in the key to create.
 * `comment` (string, optional)   
   Comment such as a name or email.
+* `key_format` (string, optional)   
+  Specify a key format. The supported key formats are: `RFC4716` (RFC 4716/SSH2 public or
+  private key), `PKCS8` (PEM PKCS8 public key) or `PEM` (PEM public key).
 * `passphrase` (string, optional, "")
   Key passphrase, empty string for no passphrase.
 * `target` (string, required)   
@@ -16,14 +19,17 @@ Generates keys for use by SSH protocol version 2.
 * `type` (string, optional, "rsa")   
   Type of key to create.
 
-## Exemple
+## Force the generation of a key compatible with SSH2
+
+For exemple in OSX Mojave, the default export format is RFC4716.
 
 ```
 require('nikita')
 .tools.ssh.keygen({
   bits: 2048,
   comment: 'my@email.com'
-  target: './id_rsa'
+  target: './id_rsa',
+  key_format: 'PEM'
 })
 
 ## Source code
@@ -32,6 +38,8 @@ require('nikita')
       options.bits ?= 4096
       options.type ?= 'rsa'
       options.passphrase ?= ''
+      options.key_format ?= null
+      throw Error "Invalid Option: key_format must be one of RFC4716, PKCS8 or PEM, got #{JSON.stringify options.key_format}" if options.key_format and options.key_format not in ['RFC4716', 'PKCS8', 'PEM']
       # Validation
       throw Error 'Required Option: target is required' unless options.target
       @system.mkdir
@@ -43,6 +51,7 @@ require('nikita')
           "-q" # Silence
           "-t #{options.type}"
           "-b #{options.bits}"
+          "-m #{options.key_format}" if options.key_format
           "-C '#{options.comment.replace '\'', '\\\''}'" if options.comment
           "-N '#{options.passphrase.replace '\'', '\\\''}'"
           "-f #{options.target}"
