16e53d3cda77a649b667d428caa53fdee304f4df
Sergei Kudinov <sergkudinov@gmail.com>
2020-11-19 17:40:47 +0100

    env: change HOME to PWD

--- packages/ipa/env/run.sh
+++ packages/ipa/env/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-HOME=`pwd`/`dirname ${BASH_SOURCE}`
+PWD=`pwd`/`dirname ${BASH_SOURCE}`
 
-npx coffee $HOME/ipa/start.coffee
+npx coffee $PWD/ipa/start.coffee
 lxc exec freeipa bash <<EOF
 cd /nikita/packages/ipa
 npm test
