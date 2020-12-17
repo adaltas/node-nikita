7797ede03c6d22ddfa302f0652a9d2a561bcd4ef
David Worms <david@adaltas.com>
2020-12-17 08:56:53 +0100

    feat(engine): new dig tool

--- packages/engine/lib/index.js
+++ packages/engine/lib/index.js
@@ -11,7 +11,7 @@ session = require('./session');
 
 create = function() {
   return session({
-    plugins: [require('./metadata/debug'), require('./metadata/depth'), require('./metadata/disabled'), require('./metadata/raw'), require('./metadata/relax'), require('./metadata/retry'), require('./metadata/status'), require('./metadata/tmpdir'), require('./plugins/args'), require('./plugins/argument'), require('./plugins/conditions'), require('./plugins/conditions_execute'), require('./plugins/conditions_exists'), require('./plugins/conditions_os'), require('./plugins/global'), require('./plugins/history'), require('./plugins/output_logs'), require('./plugins/schema'), require('./plugins/ssh'), require('./plugins/templated'), require('./plugins/tools_events'), require('./plugins/tools_find'), require('./plugins/tools_log'), require('./plugins/tools_path'), require('./plugins/tools_walk')]
+    plugins: [require('./metadata/debug'), require('./metadata/depth'), require('./metadata/disabled'), require('./metadata/raw'), require('./metadata/relax'), require('./metadata/retry'), require('./metadata/status'), require('./metadata/tmpdir'), require('./plugins/args'), require('./plugins/argument'), require('./plugins/conditions'), require('./plugins/conditions_execute'), require('./plugins/conditions_exists'), require('./plugins/conditions_os'), require('./plugins/global'), require('./plugins/history'), require('./plugins/output_logs'), require('./plugins/schema'), require('./plugins/ssh'), require('./plugins/templated'), require('./plugins/tools_dig'), require('./plugins/tools_events'), require('./plugins/tools_find'), require('./plugins/tools_log'), require('./plugins/tools_path'), require('./plugins/tools_walk')]
   }, ...arguments);
 };
 
