06997d9a120ffb56d061a81b3eacac2a639117ed
David Worms <david@adaltas.com>
2021-01-06 10:18:06 +0100

    test(env): comment centos6 env

--- packages/service/env/run.sh
+++ packages/service/env/run.sh
@@ -5,8 +5,10 @@ CWD=`pwd`/`dirname ${BASH_SOURCE}`
 cd $CWD/archlinux
 docker-compose up --abort-on-container-exit
 
-cd $CWD/centos6
-docker-compose up --abort-on-container-exit
+# CentOS 6 reach end of life,
+# we couldn't find how to get the repo working with yum update
+# cd $CWD/centos6
+# docker-compose up --abort-on-container-exit
 
 cd $CWD/centos7
 docker-compose up --abort-on-container-exit
