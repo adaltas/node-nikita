70ebb1a32486ff6b2f0a779dde3611d6d9f9fbad
Sergei Kudinov <sergkudinov@gmail.com>
2021-02-24 16:28:31 +0100

    docs: update metadata page

--- docs/content/metadata/samples/index.js
+++ docs/content/metadata/samples/index.js
@@ -1,13 +1,11 @@
 // hide-next-line
 const nikita = require('nikita');
-// Dependencies
-const assert = require('assert');
-// Call with metadata
-nikita.call({
+nikita
+// Call an action with the `header` metadata
+.execute({
   // highlight-range{1-3}
   metadata: {
-    header: 'my action'
-  }
-}, function({metadata}){
-  assert.equal(metadata.header, 'my action')
+    header: 'Check user'
+  },
+  command: 'whoami'
 })
