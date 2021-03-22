b6ab2ed874c70a77b698ecf93dd415dadfbccdb1
Sergei Kudinov <sergkudinov@gmail.com>
2021-03-22 16:35:20 +0100

    docs: fix website intro links

--- docs/website/src/components/home/Intro.js
+++ docs/website/src/components/home/Intro.js
@@ -249,7 +249,7 @@ const Intro = () => {
           <div css={styles.buttons}>
             <Button
               component={Link}
-              to="/about/tutorial/"
+              to="/current/about/tutorial/"
               size="large"
               variant="outlined"
               css={styles.button}
@@ -259,7 +259,7 @@ const Intro = () => {
             </Button>
             <Button
               component={Link}
-              to="/about/changelog/"
+              to="/current/about/changelog/"
               size="large"
               variant="outlined"
               css={styles.button}
