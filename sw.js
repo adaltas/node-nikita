810988b9272b76d643deee94f5dc08e0f9a212ec
sergkudinov <sergkudinov@gmail.com>
2021-07-01 14:33:34 +0000

    Deploying to gh-pages from  @ 217143e725c479ab88f46dda6cfa6132b15c69dc 🚀

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-c1ce326bb0f400211c56.js"
+    "url": "webpack-runtime-8c8ac25894d3025ca115.js"
   },
   {
     "url": "framework-68c705bbd7dd0549b337.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "92488be61292ca73f488a6813096b159"
+    "revision": "42accbd29fa0eff135dfee274b408b17"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-5c4f93251761eeb632de.js"
