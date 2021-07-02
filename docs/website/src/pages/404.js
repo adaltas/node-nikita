a8b43c30412fed2773037cb7ade7d3befdaf4cf8
Sergei Kudinov <sergkudinov@gmail.com>
2021-07-02 17:38:44 +0200

    docs: fix website 404 page

--- docs/website/src/pages/404.js
+++ docs/website/src/pages/404.js
@@ -9,10 +9,7 @@ const NotFoundPage = ({ data }) => (
       keywords: 'nikita, node.js, 404, not found'
     }}
   >
-    <div>
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    </div>
+    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
   </Layout>
 )
 
