eaa8d8f61c742e3aca48f5ab91cc5babe7e798c7
David Worms <david@adaltas.com>
2021-11-24 23:48:13 +0100

    test: env to call bash in run scripts

--- packages/db/env/mysql/run.sh
+++ packages/db/env/mysql/run.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 cd `pwd`/`dirname ${BASH_SOURCE}`
 docker-compose up --abort-on-container-exit
--- packages/tools/env/rubygems/run.sh
+++ packages/tools/env/rubygems/run.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -e
 
 cd `pwd`/`dirname ${BASH_SOURCE}`
