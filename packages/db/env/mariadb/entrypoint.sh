6392c2240a2b8318f48ab6346e8dc4b37d6fce30
wdavidw <david@adaltas.com>
2022-01-26 12:00:12 +0100

    test(db): user in docker images

--- packages/db/env/mariadb/entrypoint.sh
+++ packages/db/env/mariadb/entrypoint.sh
@@ -4,12 +4,12 @@ set -e
 # Source Node.js
 . ~/.bashrc
 # Start ssh daemon
-/usr/sbin/sshd
+sudo /usr/sbin/sshd
 # We have TTY, so probably an interactive container...
 if test -t 0; then
   # Some command(s) has been passed to container? Execute them and exit.
   # No commands provided? Run bash.
-  if [[ $@ ]]; then 
+  if [[ $@ ]]; then
     node_modules/.bin/mocha $@
   else
     export PS1='[\u@\h : \w]\$ '
