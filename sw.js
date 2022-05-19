e0d7073274c74b2d61f57022b4f389f50ef56eb0
wdavidw <david@adaltas.com>
2022-05-19 10:05:35 +0000

    Deploying to gh-pages from  @ 7d303bd09427a378a32f152e361b55342b55a419 🚀

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-c32e4de67aa5486d0284.js"
+    "url": "webpack-runtime-88939ff64b4e33920dae.js"
   },
   {
     "url": "framework-d1d598bd1a115a40bbda.js"
@@ -40,7 +40,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "e2672a926a6d2372f8264d8dd553c313"
+    "revision": "275e53b957ae9a40aae7378441883bf0"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-0d54da7659b276563e5c.js"
