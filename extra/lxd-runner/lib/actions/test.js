8834338a785c0046dda6dc58a96002b3e34c9689
David Worms <david@adaltas.com>
2023-01-02 23:30:26 +0100

    feat(lxd-runner): run test as user

--- extra/lxd-runner/lib/actions/test.js
+++ extra/lxd-runner/lib/actions/test.js
@@ -8,6 +8,13 @@ module.exports = function({config}) {
   return this.execute({
     stdout: process.stdout,
     env: process.env,
-    command: `lxc exec --cwd ${config.cwd} ${config.container} -- bash -l -c "npm run test:local"`
+    command: [
+      'lxc exec',
+      `--cwd ${config.cwd}`,
+      // Note, core ssh env log in as "source" user
+      config.test_user ? `--user ${config.test_user}` : void 0,
+      `${config.container} --`,
+      'bash -l -c "npm run test:local"'
+    ].join(' ')
   });
 };
--- extra/lxd-runner/src/actions/test.coffee
+++ extra/lxd-runner/src/actions/test.coffee
@@ -8,9 +8,11 @@ module.exports = ({config})->
   @execute
     stdout: process.stdout
     env: process.env
-    command: """
-    lxc exec \
-      --cwd #{config.cwd} \
-      #{config.container} -- \
-      bash -l -c "npm run test:local"
-    """
+    command: [
+      'lxc exec'
+      "--cwd #{config.cwd}"
+      # Note, core ssh env log in as "source" user
+      "--user #{config.test_user}" if config.test_user
+      "#{config.container} --"
+      'bash -l -c "npm run test:local"'
+    ].join ' '
