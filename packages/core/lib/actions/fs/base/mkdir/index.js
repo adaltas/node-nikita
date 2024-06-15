a170cac0ed807518853ff3513671ef47eb4fd1ec
David Worms <david@adaltas.com>
2024-06-15 18:52:48 +0200

    refactor(core): mkdir simplification

--- packages/core/lib/actions/fs/base/mkdir/index.js
+++ packages/core/lib/actions/fs/base/mkdir/index.js
@@ -1,40 +1,58 @@
-
 // Dependencies
-import utils from '@nikitajs/core/utils';
+import utils from "@nikitajs/core/utils";
 import definitions from "./schema.json" with { type: "json" };
 
 const errors = {
-  NIKITA_FS_MKDIR_TARGET_EEXIST: ({config}) =>
-    utils.error('NIKITA_FS_MKDIR_TARGET_EEXIST', ['fail to create a directory,', 'one already exists,', `location is ${JSON.stringify(config.target)}.`], {
-      error_code: 'EEXIST',
-      errno: -17,
-      path: config.target_tmp || config.target, // Native Node.js api doesn't provide path
-      syscall: 'mkdir'
-    })
+  NIKITA_FS_MKDIR_TARGET_EEXIST: ({ config }) =>
+    utils.error(
+      "NIKITA_FS_MKDIR_TARGET_EEXIST",
+      [
+        "fail to create a directory,",
+        "one already exists,",
+        `location is ${JSON.stringify(config.target)}.`,
+      ],
+      {
+        error_code: "EEXIST",
+        errno: -17,
+        path: config.target_tmp || config.target, // Native Node.js api doesn't provide path
+        syscall: "mkdir",
+      }
+    ),
 };
 
 // Action
 export default {
-  handler: async function({config}) {
-    if (typeof config.mode === 'number') {
+  handler: async function ({ config }) {
+    if (typeof config.mode === "number") {
       // Convert mode into a string
       config.mode = config.mode.toString(8).slice(-4);
     }
     try {
-      return (await this.execute([`[ -d '${config.target}' ] && exit 17`, ['install', config.mode ? `-m '${config.mode}'` : void 0, config.uid ? `-o '${config.uid}'` : void 0, config.gid ? `-g '${config.gid}'` : void 0, `-d '${config.target}'`].join(' ')].join('\n')));
+      return await this.execute(
+        [
+          `[ -d '${config.target}' ] && exit 17`,
+          [
+            "install",
+            config.mode && `-m '${config.mode}'`,
+            config.uid && `-o '${config.uid}'`,
+            config.gid && `-g '${config.gid}'`,
+            `-d '${config.target}'`,
+          ].filter(Boolean).join(" "),
+        ].join("\n")
+      );
     } catch (error) {
       if (error.exit_code === 17) {
         error = errors.NIKITA_FS_MKDIR_TARGET_EEXIST({
-          config: config
+          config: config,
         });
       }
       throw error;
     }
   },
   metadata: {
-    argument_to_config: 'target',
+    argument_to_config: "target",
     log: false,
     raw_output: true,
-    definitions: definitions
-  }
+    definitions: definitions,
+  },
 };
