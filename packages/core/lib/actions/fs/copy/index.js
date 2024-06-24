75a001d81c41749f6c0a00d99f17ceb1bd7649fe
David Worms <david@adaltas.com>
2024-06-24 21:07:19 +0200

    refactor(core): fs.copy simplification

--- packages/core/lib/actions/fs/copy/index.js
+++ packages/core/lib/actions/fs/copy/index.js
@@ -1,55 +1,41 @@
 // Dependencies
-import utils from '@nikitajs/core/utils';
+import utils from "@nikitajs/core/utils";
 import definitions from "./schema.json" with { type: "json" };
 
 // Action
 export default {
   handler: async function ({ config, tools: { $status, log, path } }) {
     // Retrieve stats information about the source unless provided through the "source_stats" option.
-    const source_stats = await ( async () => {
+    const source_stats = await (async () => {
       if (config.source_stats) {
-        log({
-          message: "Source Stats: using short circuit",
-          level: "DEBUG",
-        });
+        log("DEBUG", "Source Stats: using short circuit");
         return config.source_stats;
-      } else {
-        log({
-          message: `Stats source file ${config.source}`,
-          level: "DEBUG",
-        });
+      }
+      log("DEBUG", `Stats source file ${config.source}`);
+      const { stats } = await this.fs.stat({
+        target: config.source,
+      });
+      return stats;
+    })();
+    // Retrieve stat information about the traget unless provided through the "target_stats" option.
+    let target_stats = await (async () => {
+      if (config.target_stats) {
+        log("DEBUG", "Target Stats: using short circuit");
+        return config.target_stats;
+      }
+      log("DEBUG", `Stats target file ${config.target}`);
+      try {
         const { stats } = await this.fs.stat({
-          target: config.source,
+          target: config.target,
         });
         return stats;
-      }
-    })()
-    // Retrieve stat information about the traget unless provided through the "target_stats" option.
-    let target_stats = await ( async () => {
-      if (config.target_stats) {
-        log({
-          message: "Target Stats: using short circuit",
-          level: "DEBUG",
-        });
-        return config.target_stats;
-      } else {
-        log({
-          message: `Stats target file ${config.target}`,
-          level: "DEBUG",
-        });
-        try {
-          const { stats } = await this.fs.stat({
-            target: config.target,
-          });
-          return stats;
-        } catch (error) {
-          if (error.code !== "NIKITA_FS_STAT_TARGET_ENOENT") {
-            // Target file doesn't necessarily exist
-            throw error;
-          }
+      } catch (error) {
+        if (error.code !== "NIKITA_FS_STAT_TARGET_ENOENT") {
+          // Target file doesn't necessarily exist
+          throw error;
         }
       }
-    })()
+    })();
     // Create target parent directory if target does not exists and if the "parent"
     // config is set to "true" (default) or as an object.
     await this.fs.mkdir({
@@ -57,7 +43,7 @@ export default {
       $unless: target_stats,
       $shy: true,
       target: path.dirname(config.target),
-      ...config.parent
+      ...config.parent,
     });
     // Stop here if source is a directory. We traverse all its children
     // Recursively, calling either `fs.mkdir` or `fs.copy`.
@@ -82,14 +68,11 @@ export default {
             path.basename(config.source)
           );
         }
-        log({
-          message: "Source is a directory",
-          level: "INFO",
-        });
+        log("Source is a directory");
         const { files } = await this.fs.glob(`${config.source}/**`, {
           dot: true,
         });
-        for(const source of files){
+        for (const source of files) {
           await (async (source) => {
             const target = path.resolve(
               config.target,
@@ -99,32 +82,26 @@ export default {
               target: source,
             });
             let uid = config.uid;
-            if (config.preserve) {
-              if (uid == null) {
-                uid = stats.uid;
-              }
+            if (config.preserve && uid == null) {
+              uid = stats.uid;
             }
             let gid = config.gid;
-            if (config.preserve) {
-              if (gid == null) {
-                gid = stats.gid;
-              }
+            if (config.preserve && gid == null) {
+              gid = stats.gid;
             }
             let mode = config.mode;
-            if (config.preserve) {
-              if (mode == null) {
-                mode = stats.mode;
-              }
+            if (config.preserve && mode == null) {
+              mode = stats.mode;
             }
             if (utils.stats.isDirectory(stats.mode)) {
-              return await this.fs.mkdir({
+              await this.fs.mkdir({
                 target: target,
                 uid: uid,
                 gid: gid,
                 mode: mode,
               });
             } else {
-              return await this.fs.copy({
+              await this.fs.copy({
                 target: target,
                 source: source,
                 source_stats: stats,
@@ -144,20 +121,9 @@ export default {
       return res.$status;
     }
     // If source is a file and target is a directory, then transform target into a file.
-    await this.call(
-      {
-        $shy: true,
-      },
-      function () {
-        if (!(target_stats && utils.stats.isDirectory(target_stats.mode))) {
-          return;
-        }
-        return (config.target = path.resolve(
-          config.target,
-          path.basename(config.source)
-        ));
-      }
-    );
+    if (target_stats && utils.stats.isDirectory(target_stats.mode)) {
+      config.target = path.resolve(config.target, path.basename(config.source));
+    }
     // Compute the source and target hash
     const { hash: hash_source } = await this.fs.hash(config.source);
     let hash_target;
@@ -170,41 +136,29 @@ export default {
     }
     // Copy a file if content differ from source
     if (hash_source === hash_target) {
-      log({
-        message: `Hash matches as '${hash_source}'`,
-        level: "INFO",
-      });
+      log(`Hash matches as '${hash_source}'`);
     } else {
-      log({
-        message: `Hash dont match, source is '${hash_source}' and target is '${hash_target}'`,
-        level: "WARN",
-      });
+      log(
+        "WARN",
+        `Hash dont match, source is '${hash_source}' and target is '${hash_target}'`
+      );
       await this.fs.base.copy({
         source: config.source,
         target: config.target,
       });
       if ($status) {
-        log({
-          message: `File copied from ${config.source} into ${config.target}`,
-          level: "INFO",
-        });
+        log(`File copied from ${config.source} into ${config.target}`);
       }
     }
-    if (config.preserve) {
+    if (config.preserve && config.uid == null) {
       // File ownership and permissions
-      if (config.uid == null) {
-        config.uid = source_stats.uid;
-      }
+      config.uid = source_stats.uid;
     }
-    if (config.preserve) {
-      if (config.gid == null) {
-        config.gid = source_stats.gid;
-      }
+    if (config.preserve && config.gid == null) {
+      config.gid = source_stats.gid;
     }
-    if (config.preserve) {
-      if (config.mode == null) {
-        config.mode = source_stats.mode;
-      }
+    if (config.preserve && config.mode == null) {
+      config.mode = source_stats.mode;
     }
     await this.fs.chown({
       $if: config.uid != null || config.gid != null,
@@ -222,12 +176,12 @@ export default {
     return {};
   },
   hooks: {
-    on_action: function({config}) {
+    on_action: function ({ config }) {
       if (config.parent == null) {
         config.parent = {};
       }
       if (config.parent === true) {
-        return config.parent = {};
+        return (config.parent = {});
       }
     },
   },
