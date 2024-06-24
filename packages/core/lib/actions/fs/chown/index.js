e345c80142671dbd4dee782da7da69d0543ee606
David Worms <david@adaltas.com>
2024-06-25 00:20:23 +0200

    refactor(core): chown simplification

--- packages/core/lib/actions/fs/chown/index.js
+++ packages/core/lib/actions/fs/chown/index.js
@@ -4,63 +4,42 @@ import definitions from "./schema.json" with { type: "json" };
 // Action
 export default {
   handler: async function ({ config, tools: { log } }) {
-    if (!(config.uid != null || config.gid != null)) {
+    if (config.uid == null && config.gid == null) {
       throw Error("Missing one of uid or gid option");
     }
-    let uid;
-    if (config.uid != null) {
-      uid =
-        typeof config.uid === "number"
-          ? config.uid
-          : parseInt((await this.execute(`id -u '${config.uid}'`)).stdout.trim());
+    if(typeof config.uid === 'string'){
+      config.uid = await this.execute(`id -u '${config.uid}'`).then(({stdout}) => parseInt(stdout.trim()))
     }
-    let gid;
-    if (config.gid != null) {
-      gid =
-        typeof config.gid === "number"
-          ? config.gid
-          : parseInt((await this.execute(`id -g '${config.gid}'`)).stdout.trim());
+    if(typeof config.gid === 'string'){
+      config.gid = await this.execute(`id -g '${config.gid}'`).then(({stdout}) => parseInt(stdout.trim()))
     }
     // Retrieve target stats
-    let stats;
     if (config.stats) {
-      log({
-        message: "Stat short-circuit",
-        level: "DEBUG",
-      });
-      stats = config.stats;
-    } else {
-      ({ stats } = await this.fs.stat(config.target));
+      log("DEBUG", "Stat short-circuit");
     }
+    config.stats ??= await this.fs
+      .stat(config.target)
+      .then(({ stats }) => stats);
     // Detect changes
     const changes = {
-      uid: uid != null && stats.uid !== uid,
-      gid: gid != null && stats.gid !== gid,
+      uid: config.uid != null && config.stats.uid !== config.uid,
+      gid: config.gid != null && config.stats.gid !== config.gid,
     };
     if (!changes.uid && !changes.gid) {
-      log({
-        message: `Matching ownerships on '${config.target}'`,
-        level: "INFO",
-      });
+      log("INFO", `Matching ownerships on '${config.target}'`);
       return false;
     }
     // Apply changes
     await this.fs.base.chown({
       target: config.target,
-      uid: uid,
-      gid: gid,
+      uid: config.uid,
+      gid: config.gid,
     });
     if (changes.uid) {
-      log({
-        message: `change uid from ${stats.uid} to ${uid}`,
-        level: "WARN",
-      });
+      log("WARN", `change uid from ${config.stats.uid} to ${config.uid}`);
     }
     if (changes.gid) {
-      log({
-        message: `change gid from ${stats.gid} to ${gid}`,
-        level: "WARN",
-      });
+      log("WARN", `change gid from ${config.stats.gid} to ${config.gid}`);
     }
     return true;
   },
