a3e0ad43ccdd919148096f8da90a181f41c8536d
David Worms <david@adaltas.com>
2024-06-25 00:20:45 +0200

    refactor(core): fs.copy simplification

--- packages/core/lib/actions/fs/copy/index.js
+++ packages/core/lib/actions/fs/copy/index.js
@@ -12,29 +12,28 @@ export default {
         return config.source_stats;
       }
       log("DEBUG", `Stats source file ${config.source}`);
-      const { stats } = await this.fs.stat({
+      return await this.fs.stat({
         target: config.source,
-      });
-      return stats;
+      }).then( ({stats}) => stats);
     })();
     // Retrieve stat information about the traget unless provided through the "target_stats" option.
-    let target_stats = await (async () => {
+    const target_stats = await (async () => {
       if (config.target_stats) {
         log("DEBUG", "Target Stats: using short circuit");
         return config.target_stats;
       }
       log("DEBUG", `Stats target file ${config.target}`);
-      try {
-        const { stats } = await this.fs.stat({
+      return await this.fs
+        .stat({
           target: config.target,
+        })
+        .then(({ stats }) => stats)
+        .catch((error) => {
+          if (error.code !== "NIKITA_FS_STAT_TARGET_ENOENT") {
+            // Target file doesn't necessarily exist
+            throw error;
+          }
         });
-        return stats;
-      } catch (error) {
-        if (error.code !== "NIKITA_FS_STAT_TARGET_ENOENT") {
-          // Target file doesn't necessarily exist
-          throw error;
-        }
-      }
     })();
     // Create target parent directory if target does not exists and if the "parent"
     // config is set to "true" (default) or as an object.
@@ -52,73 +51,52 @@ export default {
     // "/tmp/a_target". Without an ending slash , the directory "/tmp/a_source" is
     // copied into "/tmp/a_target/a_source". With an ending slash, all the files
     // present inside "/tmp/a_source" are copied inside "/tmp/a_target".
-    const res = await this.call(
-      {
-        $shy: true,
-      },
-      async function () {
-        if (!utils.stats.isDirectory(source_stats.mode)) {
-          return;
-        }
-        const sourceEndWithSlash =
-          config.source.lastIndexOf("/") === config.source.length - 1;
-        if (target_stats && !sourceEndWithSlash) {
-          config.target = path.resolve(
-            config.target,
-            path.basename(config.source)
-          );
-        }
-        log("Source is a directory");
-        const { files } = await this.fs.glob(`${config.source}/**`, {
-          dot: true,
-        });
-        for (const source of files) {
-          await (async (source) => {
-            const target = path.resolve(
-              config.target,
-              path.relative(config.source, source)
-            );
-            const { stats } = await this.fs.stat({
-              target: source,
-            });
-            let uid = config.uid;
-            if (config.preserve && uid == null) {
-              uid = stats.uid;
-            }
-            let gid = config.gid;
-            if (config.preserve && gid == null) {
-              gid = stats.gid;
-            }
-            let mode = config.mode;
-            if (config.preserve && mode == null) {
-              mode = stats.mode;
-            }
-            if (utils.stats.isDirectory(stats.mode)) {
-              await this.fs.mkdir({
-                target: target,
-                uid: uid,
-                gid: gid,
-                mode: mode,
-              });
-            } else {
-              await this.fs.copy({
-                target: target,
-                source: source,
-                source_stats: stats,
-                uid: uid,
-                gid: gid,
-                mode: mode,
-              });
-            }
-          })(source);
-        }
-        return {
-          end: true,
-        };
+    if (utils.stats.isDirectory(source_stats.mode)) {
+      const sourceEndWithSlash =
+        config.source.lastIndexOf("/") === config.source.length - 1;
+      if (target_stats && !sourceEndWithSlash) {
+        config.target = path.resolve(
+          config.target,
+          path.basename(config.source)
+        );
       }
-    );
-    if (res.end) {
-      return res.$status;
+      log("Source is a directory");
+      const { files } = await this.fs.glob(`${config.source}/**`, {
+        dot: true,
+      });
+      for (const source of files) {
+        const target = path.resolve(
+          config.target,
+          path.relative(config.source, source)
+        );
+        const { stats } = await this.fs.stat({
+          target: source,
+        });
+        const uid =
+          config.preserve && config.uid == null ? stats.uid : config.uid;
+        const gid =
+          config.preserve && config.gid == null ? stats.gid : config.gid;
+        const mode =
+          config.preserve && config.mode == null ? stats.mode : config.mode;
+        if (utils.stats.isDirectory(stats.mode)) {
+          await this.fs.mkdir({
+            target: target,
+            uid: uid,
+            gid: gid,
+            mode: mode,
+          });
+        } else {
+          await this.fs.copy({
+            target: target,
+            source: source,
+            source_stats: stats,
+            uid: uid,
+            gid: gid,
+            mode: mode,
+          });
+        }
+      }
+      return;
     }
     // If source is a file and target is a directory, then transform target into a file.
     if (target_stats && utils.stats.isDirectory(target_stats.mode)) {
@@ -126,14 +104,14 @@ export default {
     }
     // Compute the source and target hash
     const { hash: hash_source } = await this.fs.hash(config.source);
-    let hash_target;
-    try {
-      ({ hash: hash_target } = await this.fs.hash(config.target));
-    } catch (error) {
-      if (error.code !== "NIKITA_FS_STAT_TARGET_ENOENT") {
+    const { hash: hash_target } = await this.fs
+      .hash(config.target)
+      .catch((error) => {
+        if (error.code === "NIKITA_FS_STAT_TARGET_ENOENT") {
+          return {};
+        }
         throw error;
-      }
-    }
+      });
     // Copy a file if content differ from source
     if (hash_source === hash_target) {
       log(`Hash matches as '${hash_source}'`);
@@ -150,36 +128,29 @@ export default {
         log(`File copied from ${config.source} into ${config.target}`);
       }
     }
-    if (config.preserve && config.uid == null) {
-      // File ownership and permissions
-      config.uid = source_stats.uid;
-    }
-    if (config.preserve && config.gid == null) {
-      config.gid = source_stats.gid;
-    }
-    if (config.preserve && config.mode == null) {
-      config.mode = source_stats.mode;
-    }
     await this.fs.chown({
       $if: config.uid != null || config.gid != null,
       target: config.target,
       stats: target_stats,
-      uid: config.uid,
-      gid: config.gid,
+      uid:
+        config.preserve && config.uid == null ? source_stats.uid : config.uid,
+      gid:
+        config.preserve && config.gid == null ? source_stats.gid : config.gid,
     });
     await this.fs.chmod({
       $if: config.mode != null,
       target: config.target,
       stats: target_stats,
-      mode: config.mode,
+      mode:
+        config.preserve && config.mode == null
+          ? source_stats.mode
+          : config.mode,
     });
     return {};
   },
   hooks: {
     on_action: function ({ config }) {
-      if (config.parent == null) {
-        config.parent = {};
-      }
+      config.parent ??= {};
       if (config.parent === true) {
         return (config.parent = {});
       }
