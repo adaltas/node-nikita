7bd458ac6daa18f45945086b52f138d9e5aef219
David Worms <david@adaltas.com>
2024-11-16 16:02:16 +0100

    build: eslint mocha rules

--- eslint.config.js
+++ eslint.config.js
@@ -13,4 +13,12 @@ export default [
   js.configs.recommended,
   mocha.configs.flat.recommended,
   prettier,
+  {
+    rules: {
+      // Setup for test cases in mocha should be done in before, beforeEach,
+      // or it blocks. Unfortunately there is nothing stopping you from doing
+      // setup directly inside a describe block.
+      "mocha/no-setup-in-describe": "off",
+    },
+  },
 ];
